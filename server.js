25da80bd79855811bc3ac55d234691a1f6f24308
Luuca12 <162985674+Luuca12@users.noreply.github.com>
2025-06-12 11:04:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+// server.js
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/contact", (req, res) => {
+    const { name, email, message } = req.body;
+    console.log("Form submission:", name, email, message);
+    res.status(200).send("Message received!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
 
